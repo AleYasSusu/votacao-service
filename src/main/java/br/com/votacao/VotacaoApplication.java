617929210530c3bcb7905aaa84d052f3478f6af6
Alessandro <alessandro.souza@edu.pucrs.br>
2023-05-21 23:44:43 -0300

    alteracao das config docker

--- src/main/java/br/com/votacao/VotacaoApplication.java
+++ src/main/java/br/com/votacao/VotacaoApplication.java
@@ -1,11 +1,9 @@
 package br.com.votacao;
 
-import br.com.votacao.service.VoteService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.client.RestTemplate;
 
--- src/main/java/br/com/votacao/exception/CpfValidationException.java
+++ src/main/java/br/com/votacao/exception/CpfValidationException.java
@@ -1,7 +1,5 @@
 package br.com.votacao.exception;
 
-import org.springframework.http.HttpStatus;
-
  public class CpfValidationException extends RuntimeException {
     public CpfValidationException(String message) {
         super(message);
